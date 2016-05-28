82696a5d0933b231f73bcd3598a4e8e5c1dea80e
Horosphere <henrywen99@gmail.com>
2016-05-27 19:54:32 -0700

    Setup compiling scripts

---
+++ thk.cpp
@@ -0,0 +1,19 @@
+#include "thk.hpp"
+
+URHO3D_DEFINE_APPLICATION_MAIN(THKame)
+
+THKame::THKame(Urho3D::Context* context): Urho3D::Application(context)
+{
+}
+
+void THKame::Setup()
+{
+}
+
+void THKame::Start()
+{
+}
+
+void THKame::Stop()
+{
+}
---
+++ thk.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Urho3D/Engine/Application.h>
+
+class THKame final: public Urho3D::Application
+{
+	URHO3D_OBJECT(THKame, Urho3D::Application)
+
+public:
+	THKame(Urho3D::Context* context);
+
+	virtual void Setup() override;
+	virtual void Start() override;
+	virtual void Stop() override;
+};
+
