9ccfe4f7f908d6d6213d438f29d7e69113f109c0
Horosphere <henrywen99@gmail.com>
2016-05-30 21:37:02 -0700

    Add script for auto-generating source files.

--- README.md
+++ README.md
@@ -20,3 +20,17 @@ cmake CMakeLists.txt -DLIBDIR_URHO3D:STRING=<Urho3D source directory>
 I highly suggest you make the above two commands into a script.
 
 If you are using GNU Makefiles as your generator, `make` can now be ran to produce the desired binary.
+
+## Development
+
+Each time a new source file is introduced, please run `update.py` to add it to `CMakeLists.txt`
+
+The source files are formatted according to the following `astyle` configuration:
+```
+--style=allman
+--indent=tab
+--indent-preproc-block
+--pad-header
+--unpad-paren
+--align-pointer=type	
+```
---
+++ update.py
@@ -0,0 +1,53 @@
+# Execute this script each time you add a new .cpp files in the sources to
+# update CMakeLists.txt
+
+fileName = "CMakeLists.txt"
+signature_begin = "# Auto-generated. Do not edit. All changes will be undone\n"
+signature_end = "# Auto-generated end\n"
+autoGen_begin = "set(SOURCE_FILES\n"
+autoGen_padding = "    ${PROJECT_SOURCE_DIR}"
+autoGen_end = "   )\n"
+
+import os, sys
+
+cMakeFile = [] # Lines of CMakeLists.txt
+iBegin = -1 # Indices corresponding to auto-generated sections
+iEnd = -1
+with open(fileName, "r") as file:
+    index = 0
+    for line in file:
+        cMakeFile.append(line)
+        if line == signature_begin:
+            iBegin = index
+        elif line == signature_end:
+            iEnd = index
+        index += 1
+
+if iBegin == -1 or iEnd == -1 or iBegin > iEnd:
+    print("No auto-generated region found in CMakeLists.txt.")
+    print("Please check that the following lines exist:")
+    print(signature_begin[:-1])
+    print(signature_end[:-1])
+    sys.exit(1)
+
+del cMakeFile[iBegin + 1: iEnd]
+
+def isCPPSource(name):
+    f
+autoGen = autoGen_begin
+# Scan src/ directory recursively to gather source files
+for root, subdirs, files in os.walk("./src"): # The root string has length 5
+    for name in files:
+        f = str(os.path.join(root, name))[5:]
+        if not f.endswith(('.cpp', '.c')): continue
+        print(f)
+        autoGen += autoGen_padding + f + "\n"
+
+autoGen += autoGen_end
+cMakeFile.insert(iBegin + 1, autoGen)
+
+with open(fileName, "w") as file:
+    for line in cMakeFile:
+        file.write(line)
+
+
